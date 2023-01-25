9d8458f975d3cbcaf28063b65cd38e669f1c6c89
hassan kazungu <hassankazungu01@gmail.com>
2023-01-25 19:17:51 +0300

    done

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "whatwg-fetch";
 import {
   fireEvent,
   render,
@@ -7,7 +6,7 @@ import {
   waitForElementToBeRemoved,
 } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { server } from "../mocks/server";
+import { server } from "./mocks/server";
 
 import App from "../components/App";
 
